f9c8f749e91e7760d4f705bc80043947794098c6
kaweesa simon peter <86351444+simonpeterfuture@users.noreply.github.com>
2024-06-07 10:53:45 +0000

    homework_03

---
+++ mlops/homework_03/transformers/transformer_block.py
@@ -0,0 +1,25 @@
+import pandas as pd 
+
+if 'transformer' not in globals():
+    from mage_ai.data_preparation.decorators import transformer
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+
+@transformer
+def transform(df: pd.DataFrame, **kwargs) -> pd.DataFrame:
+
+    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+
+    df['duration'] = df.tpep_dropoff_datetime - df.tpep_pickup_datetime
+    df.duration = df.duration.dt.total_seconds() / 60
+
+    df = df[(df.duration >= 1) & (df.duration <= 60)]
+
+    categorical = ['PULocationID', 'DOLocationID']
+    df[categorical] = df[categorical].astype(str)
+    
+    # Specify your transformation logic here
+
+    return df
